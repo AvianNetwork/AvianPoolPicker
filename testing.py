ba0a962a7f9a8ad5c07c1efd9a6a8c93ad2fff5d
primboi <prianshmittra@gmail.com>
2022-01-21 19:37:23 -0800

    damn, it does not work

--- testing.py
+++ testing.py
@@ -3,3 +3,4 @@ import platform
 GPU = pools.GPU()
 print(GPU.idworkersGPU(pool="ZergPool", id="I_Love_Zergpool"))
 print(GPU.get_miners(miner="T-Rex Miner", platform=platform.system()))
+print(GPU.get_password(pool="rplant"))
